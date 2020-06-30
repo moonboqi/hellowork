5204adce2ac94fcfbd6cd5dde4fc4e4a064b73f4
TENCENT\alexmnwang <moonboqi1@sina.com>
2020-06-30 11:23:42 +0800

    更新6-30-1

--- src/hellowork/HelloWorld.java
+++ src/hellowork/HelloWorld.java
@@ -3,5 +3,9 @@ package hellowork;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
+		System.out.println("HelloWorld1");
+		System.out.println("HelloWorld2");
+		System.out.println("HelloWorld3");
+		System.out.println("HelloWorld4");
 	}
 }
